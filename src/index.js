bc12ed7fb52fc9ae63ca5ddfc49ccaaf26ee5053
Andrew <aforest41@yahoo.com>
2022-12-01 20:56:47 -0800

    commit tree d32a0157cad95ce7902e26f38a7e3da48a97d554

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
 const init = () => {
-  
+    const inputForm = document.querySelector('form');
+    inputForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const input = event.target.children[1].value
+    
+
+        fetch(`http://localhost:3000/movies/${input}`)
+        .then(response => response.json())
+        .then(data => {
+        const movieDetails = document.getElementById("movieDetails");
+        movieDetails.innerHTML = `
+        <h4>${data.title}</h4>
+        <p>${data.summary}</p>`
+        });
+
+    });
 }
 
 document.addEventListener('DOMContentLoaded', init);
